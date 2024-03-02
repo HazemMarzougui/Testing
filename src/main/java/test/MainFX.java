2a244dc1bec13cd57dc8078d9d6fa661a4b332f4
Hazem Marzougui <hazem.marzougui@esprit.tn>
2024-03-02 15:31:21 +0100

    final

--- src/main/java/test/MainFX.java
+++ src/main/java/test/MainFX.java
@@ -1,48 +1,42 @@
 package test;
 
-import entities.panier;
+import entities.Utilisateur;
 import javafx.application.Application;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import services.UtilisateurServices;
+import utils.MyDB;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class MainFX extends Application {
-
+import java.io.IOException;
 
 
+public class MainFx extends Application {
+    private Parent fxml;
+    private VBox vbox;
+    public static int page=0;
+    public static Utilisateur connecteduser = new Utilisateur();
 
-    public static class GlobalData {
-        public static List<Integer> produits = new ArrayList<>();
-        public static List<Float> prix = new ArrayList<>();
-        public static List<Integer> quantites = new ArrayList<>();
-        public static List<panier> PanierProCom = new ArrayList<>();
-        public static List<panier> CommandeC = new ArrayList<>();
+    public static int m = 0;
+    public static int idd = 0;
 
-        public static int globalLong = 0;
-
-
-    }
     @Override
-    public void start(Stage stage) throws Exception {
-            //FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("/dashboard.fxml"));
-            FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("/front office.fxml"));
 
-        Parent root= fxmlLoader.load();
-        Scene scene= new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("Gerer commande");
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        MyDB db = MyDB.getInstance();
+        System.out.println(db);
+        Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
+
     }
-
-
-
-
 
     public static void main(String[] args) {
         launch(args);
